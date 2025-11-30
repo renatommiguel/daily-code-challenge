fc377f9200f3655b31f768a1d213df789298c323
renatommiguel <renatommiguel@gmail.com>
2025-11-30 10:28:42 +0100

    not completed yet

--- free-code-camp/python/dcc-20251128.py
+++ free-code-camp/python/dcc-20251128.py
@@ -1,27 +1,3 @@
-"""
-Word Guesser
-Given two strings of the same length, a secret word and a guess, compare the guess to the 
-secret word using the following rules:
-
-The secret word and guess will only consist of uppercase letters ("A" to "Z");
-For each letter in the guess, replace it with a number according to how it matches the secret word:
-"2" if the letter is in the secret word and in the correct position.
-"1" if the letter is in the secret word but in the wrong position.
-"0" if the letter is not in the secret word.
-Each letter in the secret word can be used at most once.
-Exact matches ("2") are assigned first, then partial matches ("1") are assigned from left to right for 
-remaining letters.
-If a letter occurs multiple times in the guess, it can only match as many times as it appears in 
-the secret word.
-For example, given a secret word of "APPLE" and a guess of "POPPA", return "10201":
-
-The first "P" is not in the correct location ("1"), the "O" isn't in the secret word ("0"), 
-the second "P" is in the correct location ("2"), the third "P" is a zero ("0") 
-because the two "P"'s in the secret word have been used, and the "A" is not in the correct location ("1").
-"""
-
-
-
 def compare(word, guess):
     if not isinstance(word,str):
         return False
@@ -34,22 +10,25 @@ def compare(word, guess):
     unique_letters = set(word)
     length = len(word)
     res = length * [0]
+    dic = {l:{'wc':word.count(l), 'gc':0,'l':l} for l in list(set(word))}
+    new_word=""
     for ind, letter in enumerate(guess):
+        if letter in word:
+            if word[ind] == guess[ind]:
+                dic[letter]['gc']+=+1
+                res[ind]=2
 
-        if word[ind] == guess[ind]:
-            res[ind]=2
+                
+    for ind, letter in enumerate(guess):
+        if letter in word:
+            dic[letter]['gc'] = dic[letter]['gc']+1
+            if not dic[letter]['gc']>2 and res[ind]!=2:
+                    res[ind]=1
 
     # x = {l:{'count':word.count(l),"first_index":word.index(l)} for l in list(set(word))}
-
-
-            
-    
-    return res
-comp = compare("WIRELESS", "ETHERNET")
-print(comp)
-wire = "WIRELESS"
-
-print(sorted(wire))
-# print(list(set(wire)))
-# print(wire.count("E"))
-# # print(dir(wire))
+    w = ''
+    for x in res:
+        w = w+str(x)
+    print(w)
+    return w
+compare("JAVASCRIPT", "TYPESCRIPT")
