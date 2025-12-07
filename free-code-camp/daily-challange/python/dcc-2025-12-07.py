0b8daa1d120fdf1bdc9b363c55c22cd440955a3f
renatommiguel <renatommiguel@gmail.com>
2025-12-07 22:25:06 +0100

    wip

--- free-code-camp/daily-challange/python/dcc-2025-12-07.py
+++ free-code-camp/daily-challange/python/dcc-2025-12-07.py
@@ -1,3 +1,11 @@
+"""
+String Compression
+Given a string sentence, return a compressed version of the sentence where consecutive duplicate words are replaced by the word followed with the number of times it repeats in parentheses.
+
+Only consecutive duplicates are compressed.
+Words are separated by single spaces.
+For example, given "yes yes yes please", return "yes(3) please".
+"""
 def compress_string(sentence):
     sentence_split = sentence.split(" ")
     uniq_words = list(dict.fromkeys(sentence_split))
