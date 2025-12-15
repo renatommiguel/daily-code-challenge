d5f3e7b445b96cb4a814a7d586f012ec5d7fb765
renatommiguel <renatommiguel@gmail.com>
2025-12-15 15:55:31 +0100

    aoc1

---
+++ 2025-advent-of-code/python/aoc1/aoc1.py
@@ -0,0 +1,29 @@
+l:list = list(range(0,100))
+values = ["L68","L30","R48","L5","R60","L55","L1","L99","R14","L82"]
+
+with open("aoc2025/aoc1/aoc1-puzzle-input.txt", 'r') as file:
+            # Read all lines from the file into a list of strings
+            lines_list = file.readlines()
+values = lines_list
+ind:int = 50
+counter:int = 0 
+for v in values:
+    if v.startswith("L"):
+        n = int(v[1:])
+        # subtract from the index
+        n = int(v[1:])
+        ind-=n
+    else:
+        # add to the index
+        n = int(v[1:])
+        ind+=n
+    if ind > 99:# or ind <-100:
+        ind = ind%100
+    elif  ind <-100:
+        ind = -(abs(ind)%100)
+    ind = l[ind]
+    if ind == 0:
+        counter+=1
+print(counter)    
+
+              
